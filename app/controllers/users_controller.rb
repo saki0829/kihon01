739dbf3d572fb46a5b3fab2ae35c399894d9c2ab
saki0829 <mmj0829@gmail.com>
2022-04-04 22:34:36 +0900

    01

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    skip_before_action :login_required
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to login_path
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        logout
+        redirect_to root_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :last_name, :first_name)
+    end
+end
